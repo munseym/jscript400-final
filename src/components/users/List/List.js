de6ec944b3c4a157f5ec3c6bf8a4f9de10c4ff97
Michael Munsey <michael.r.munsey@boeing.com>
2019-08-28 10:59:42 -0700

    All students view for students works

--- src/components/users/List/List.js
+++ src/components/users/List/List.js
@@ -2,20 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default ({ users }) => {
-  const lis = users.map(user => (
-    <li key={user._id}>
-      <Link to={`/users/${user._id}/posts`}>
-        {user.username}: {user.posts.length} Posts
-      </Link>
-    </li>
+  const students = users.map(student => (
+    <div key={student._id} className='card bg-light mb-4'>
+      <div className='d-flex flex-row justify-content-between'>
+        <div>
+          <span className='font-weight-bold'>{student.firstName} {student.lastName}</span>
+          <span> - {student.email}</span>
+        </div>
+      </div>
+    </div>
   ))
 
   return (
     <>
-      <h1>All Assignments</h1>
-      <ul>
-        { lis }
-      </ul>
+      {students}
     </>
   )
 }
